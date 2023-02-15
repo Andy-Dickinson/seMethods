30ccfa01b65fe041526592f6e3d7d0c74c00a56b
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-02-15 15:24:20 +0000

    Variable added to easily change testing on db or localhost

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,11 @@ import java.sql.*;
 
 public class App
 {
+    /* Ensure set to false before pushing to GitHub
+   If setting to false and testing locally, start db first before running app
+    */
+    private Boolean test_on_localhost = false;
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -11,6 +16,12 @@ public class App
 
         // Connect to database
         a.connect();
+
+//        // Get Employee by ID
+//        Employee emp = a.getEmployee(255530);
+//        // Display results
+//        a.displayEmployee(emp);
+
 
         // Test method to print all employees first names
         a.getEmployees();
@@ -55,9 +66,11 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name" //", salaries.salary "
                             + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                            //+ "INNER JOIN salaries"
+                            //+ "ON employees.emp_no = salaries.emp_no"
+                            + "WHERE employees.emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -65,9 +78,10 @@ public class App
             if (rset.next())
             {
                 Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
                 return emp;
             }
             else
@@ -78,6 +92,25 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
+        }
+    }
+
+    /**
+     * Method to display employee by ID
+     * @param emp
+     */
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 
@@ -108,13 +141,18 @@ public class App
             System.out.println("Connecting to database...");
             try
             {
+                int delay = 30000;
+                String port = "3306";
+                if (test_on_localhost) {
+                    delay = 0;
+                    port = "33060";
+                }
                 // Wait a bit for db to start
-                Thread.sleep(30000); // Change delay to 30000 before pushing to GitHub, set to 0 when db up and running and testing locally
+                Thread.sleep(delay); // Change delay to 30000 before pushing to GitHub, set to 0 when db up and running and testing locally
                 // Connect to database
                 //docker use db:3306
                 //local use localhost:30060
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-//                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:" + port + "/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
